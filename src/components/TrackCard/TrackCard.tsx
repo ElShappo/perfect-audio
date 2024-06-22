249031b64b40dd8f2e4de74cf69a103ede9f82a9
Deni <deni.shaipp@gmail.com>
2024-06-22 10:39:45 +0300

    add sample mp3 file + continue implementing TrackCard

--- src/components/TrackCard/TrackCard.tsx
+++ src/components/TrackCard/TrackCard.tsx
@@ -1,5 +1,6 @@
 import { Card, Image } from "@vkontakte/vkui";
 import CardBasic from "../../assets/card_basic.svg";
+import SampleAudio from "../../assets/keys-of-moon-the-success(chosic.com).mp3";
 import "./TrackCard.css";
 
 type TrackCardProps = {
@@ -10,11 +11,17 @@ type TrackCardProps = {
 };
 const TrackCard = ({ name, author, duration, image }: TrackCardProps) => {
   return (
-    <Card style={{ display: "flex" }}>
+    <Card style={{ display: "flex", alignItems: "center", gap: "1em" }}>
       <Image src={CardBasic} size={80}></Image>
       <div>
         <h2>{name}</h2>
         <h3>{author}</h3>
+      </div>
+      <div>
+        <audio controls>
+          <source src={SampleAudio} type="audio/mp3" />
+          Ваш бразуер не поддерживает воспроизведение аудио
+        </audio>
       </div>
     </Card>
   );
